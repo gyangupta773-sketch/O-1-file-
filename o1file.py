cb273ff1eaf45a1d1c220f315f04727d80c75164
gyangupta773-sketch <gyangupta773@gmail.com>
2026-03-17 17:31:36 +0530

    Add files via upload

---
+++ o1file.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iterations = 0
+    print("The number entered by the user is: ", n )
+    iterations += 6
+    print("The total iterations done by the codeis: ", iterations)
+
+    printnumber(10)
+    printnumber(10)
